146ab5cc45c3cb55dcdd0333d0ec16c3e16ec7d7
ANTARES_SEO <75557859+ANTARES-KOR@users.noreply.github.com>
2022-03-27 23:54:39 +0900

    docker section 5

---
+++ Docker/section5/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello client!");
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Running Server on ${HOST}:${PORT}`);
